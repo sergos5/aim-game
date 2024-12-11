80cf0408af549b14e85526161c2b24e2094b7631
Sergey Opekunov <sergos5@yandex.ru>
2024-12-11 20:18:11 +0300

    закончил основной функционал

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ const timer = document.querySelector('#time')
 const board = document.querySelector('#board')
 
 let time = 0
+let score = 0
 let interval
 
 const startGame = () => {
-    interval = setInterval(decreaseTime, 100000)
+    interval = setInterval(decreaseTime, 1000)
     createRandomCircle()
     showTime(time)
 }
@@ -33,14 +34,27 @@ const showTime = (value) => {
 
 const finichGame = () => {
     clearInterval(interval)
+    timer.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
 }
 
 const createRandomCircle = () => {
     const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const { width, height } = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+
     circle.classList.add('circle')
-    circle.style.width = '20px'
-    circle.style.height = '20px'
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.left = `${x}px`
+    circle.style.top = `${y}px`
     board.append(circle)
+}
+
+const getRandomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
 }
 
 startBtn.addEventListener('click', (event) => {
@@ -56,6 +70,13 @@ timeBtns.addEventListener('click', (event) => {
     }
 })
 
+board.addEventListener('click', (event) => {
+    if (event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
 
 
 
