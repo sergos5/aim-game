11121b2a31cdd0b57a13dfd2f11fd5686b964efe
Sergey Opekunov <sergos5@yandex.ru>
2024-12-11 20:32:08 +0300

    добавл кнопку и разные цвета кругам

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const startBtn = document.querySelector('#start')
 const timeBtns = document.querySelector('#time-list')
 const timer = document.querySelector('#time')
 const board = document.querySelector('#board')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', 'e67e22', '#2eec71']
 
 let time = 0
 let score = 0
@@ -24,7 +25,6 @@ const decreaseTime = () => {
     } else if (current < 10) {
         current = `0${current}`
     }
-    console.log(current);
     showTime(current)
 }
 
@@ -50,11 +50,16 @@ const createRandomCircle = () => {
     circle.style.height = `${size}px`
     circle.style.left = `${x}px`
     circle.style.top = `${y}px`
+    circle.style.background = getRandomColor()
     board.append(circle)
 }
 
 const getRandomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min) + min)
+}
+
+const getRandomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)]
 }
 
 startBtn.addEventListener('click', (event) => {
